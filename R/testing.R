007663454aeffd271c27ed6b6361d4acb5947c23
Coralie <coralie.williams@unsw.edu.au>
2022-11-07 12:06:15 +1100

    update testing script

--- R/testing.R
+++ R/testing.R
@@ -12,7 +12,7 @@
 
 
 
-#### Load functions
+#### Load functions -------------
 # Set path where the function script is saved
 funcDir <- "C:/Users/z5394590/OneDrive - UNSW/Documents/Projects/splitref/R"
 
@@ -20,19 +20,23 @@ funcDir <- "C:/Users/z5394590/OneDrive - UNSW/Documents/Projects/splitref/R"
 source(file.path(funcDir, "functions_splitref.R"))
 
 
+#### Get file path ----------------
 
 # Set root directory and paths where to save files
-datdir <- rprojroot::find_rstudio_root_file("splitRef")
+datdir <- rprojroot::find_rstudio_root_file("data")
 
 # Obtain path of csv file exported from Rayyan with full reference list (make sure to include name)
 filePath <- file.path(datdir, "articles.csv")
 
 
-#### getpilotref function 
+
+#### getpilotref function test --------------
 getpilotref(filePath, n=15, fileName="pilot_test")
 
 
-#### splitref function 
+
+#### splitref function test -----------------
+
 # Test 1: simple equal split
 splitref(filePath, n=2, prop=0.5, seed=123, fileName="split_test1")
 
